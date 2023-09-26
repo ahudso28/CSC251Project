8b6e4bf88d528ac9ab9d2ad4076072818b8cea43
Alec Hudson <143360539+ahudso28@users.noreply.github.com>
2023-09-26 00:13:33 -0400

    Added static field to track number of policies created.

--- Project_Alec_Hudson.java
+++ Project_Alec_Hudson.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.util.*;
 public class Project_Alec_Hudson
 {
+   private static int numberPolicies = 0, numberSmokers = 0, numberNonSmokers = 0;
+   
    public static void main(String[] args) throws IOException
    {      
       File file = new File("PolicyInformation.txt");
@@ -16,21 +18,22 @@ public class Project_Alec_Hudson
       ArrayList<Policy> policies = new ArrayList<Policy>();
       
       while (input.hasNext())
-      {
+      {           
          Policy policy = new Policy();
          
          policy.setPolicyNumber( input.nextInt() );
          if (input.hasNext())
             input.nextLine();
          policy.setProvidersName( input.nextLine() );
-         policy.setPolicyholdersFirstName( input.nextLine() );
-         policy.setPolicyholdersLastName( input.nextLine() );
-         policy.setPolicyholdersAge( input.nextInt() );
+         policy.setPolicyHolder( new PolicyHolder() );
+         policy.getPolicyHolder().setPolicyholdersFirstName( input.nextLine() );
+         policy.getPolicyHolder().setPolicyholdersLastName( input.nextLine() );
+         policy.getPolicyHolder().setPolicyholdersAge( input.nextInt() );
          if (input.hasNext())
             input.nextLine();
-         policy.setPolicyholdersSmokingStatus( input.nextLine() );
-         policy.setPolicyholdersHeight( input.nextDouble() );
-         policy.setPolicyholdersWeight( input.nextDouble() );
+         policy.getPolicyHolder().setPolicyholdersSmokingStatus( input.nextLine() );
+         policy.getPolicyHolder().setPolicyholdersHeight( input.nextDouble() );
+         policy.getPolicyHolder().setPolicyholdersWeight( input.nextDouble() );
          
          policies.add(policy);
          
@@ -47,35 +50,24 @@ public class Project_Alec_Hudson
       
    public static void displayInformation(ArrayList<Policy> policies)
    {  
-      int numberSmokers = 0, numberNonSmokers = 0;
-      
       for (int index = 0; index < policies.size(); index++)
       {
          Policy policy = policies.get(index);
          
-         System.out.print(
-            "Policy Number: " + policy.getPolicyNumber()
-            + "\nProvider's Name: " + policy.getProvidersName()
-            + "\nPolicyholder's First Name: " + policy.getPolicyholdersFirstName()
-            + "\nPolicyholder's Last Name: " + policy.getPolicyholdersLastName()
-            + "\nPolicyholder's Age: " + policy.getPolicyholdersAge()
-            + "\npolicyholder's Smoking Status: " + policy.getPolicyholdersSmokingStatus()
-         );
-         System.out.printf("\nPolicyholder's Height: %.1f inches", policy.getPolicyholdersHeight() );
-         System.out.printf("\nPolicyholder's Weight: %.1f pounds", policy.getPolicyholdersWeight() );
-         System.out.printf("\nPolicyholder's BMI: %.2f", policy.calculateBMI() );
-         System.out.printf("\nPolicy Price: $%.2f", policy.calculatePrice() );
+         System.out.print( policy.toString() );
          
-         if (policy.getPolicyholdersSmokingStatus().equalsIgnoreCase("smoker"))
-            numberSmokers += 1;
-         else if (policy.getPolicyholdersSmokingStatus().equalsIgnoreCase("non-smoker"))
-            numberNonSmokers += 1;
+         numberPolicies++;
+         if (policy.getPolicyHolder().getPolicyholdersSmokingStatus().equalsIgnoreCase("smoker"))
+            numberSmokers++;
+         else if (policy.getPolicyHolder().getPolicyholdersSmokingStatus().equalsIgnoreCase("non-smoker"))
+            numberNonSmokers++;
          
          if (index < policies.size())
             System.out.println("\n");
       }
       
-      System.out.print("The number of policies with a smoker is: " + numberSmokers
+      System.out.print("There were " + numberPolicies + " Policy objects created."
+      + "\nThe number of policies with a smoker is: " + numberSmokers
       + "\nThe number of policies with a non-smoker is: " + numberNonSmokers);
    }
 }
